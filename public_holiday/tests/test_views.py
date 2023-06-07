8983a541c1d0f3282c9e32ce7db74876fc04bbfa
t04st3r <15409651@life.hkbu.edu.hk>
2023-06-08 01:36:48 +0200

    improved coverage

--- public_holiday/tests/test_views.py
+++ public_holiday/tests/test_views.py
@@ -1,6 +1,5 @@
 import pytest
 
-from django.conf import settings
 from django.urls import reverse
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
@@ -9,31 +8,35 @@ from rest_framework.test import APIClient
 from unittest import mock
 from .factories import PublicHolidayFactory
 
-def raise_():
-    raise KafkaError
-
 class Bunch:
     def __init__(self, **kwds):
         self.__dict__.update(kwds)
+
+def raise_():
+    raise KafkaError
+
+def get_metadata_():
+    return Bunch(topic="some_topic", partition=1, offset=0)
+
 
 @pytest.mark.django_db
 class TestProducerAPI:
 
     def test_producer_success(self):
-        PublicHolidayFactory.create_batch(10)
-        client = APIClient()
-        response = client.get(reverse("producer"))
+        future = Bunch(get=lambda timeout: get_metadata_())
+        with mock.patch.object(KafkaProducer, 'send', return_value=future):
+            PublicHolidayFactory.create_batch(10)
+            client = APIClient()
+            response = client.get(reverse("producer"))
         assert response.status_code == status.HTTP_200_OK
         response_dict = response.json()
         assert len(response_dict["data"]) == 10
         assert response_dict["result"] == "OK"
-        assert response_dict["topic"] == settings.KAFKA_BROKER_TOPIC
 
 
     def test_producer_error(self):
         future = Bunch(get=lambda timeout: raise_())
         with mock.patch.object(KafkaProducer, 'send', return_value=future):
-            PublicHolidayFactory.create_batch(10)
             client = APIClient()
             response = client.get(reverse("producer"))
         assert response.status_code == status.HTTP_503_SERVICE_UNAVAILABLE
